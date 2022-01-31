464b08e808f1c12bf849577cae74c8af95c7ab18
Virginia Gutiérrez <virginiagutierrez1986@gmail.com>
2022-01-31 16:23:20 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './assets/App.css';
+import Navbar from './components/Navbar';
+import UserList from './components/UserList';
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <UserList />
+    </Provider>
+  );
+}
+
+export default App;
